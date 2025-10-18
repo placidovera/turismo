cfd6bfe8dac0cbc0a79a78a32c832624fc21528e
placidovera <buchetstefania@hotmail.com.ar>
2025-10-18 18:28:11 -0300

    menu hambuerguesa

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -4,16 +4,38 @@ const NavBar = ({ cambiarEstado }) => {
   return (
     <nav className="navbar navbar-expand-lg custom-navbar">
       <div className="container-fluid">
-        <h1>Paquetes Turisticos</h1>
-      </div>
+        <h1 className="navbar-brand">Paquetes Turísticos</h1>
 
-      <div className="collapse navbar-collapse justify-content-end">
-        <ul className="navbar-nav text-center">
-          <li>Vuelos</li>
-          <li>Bus</li>
-          <li>Pack</li>
-          <li className="botonNav" onClick={cambiarEstado}>Contactanos</li>
-        </ul>
+        {/* Botón hamburguesa (toggler) */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Contenedor colapsable */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav text-center">
+            <li className="nav-item">
+              <a className="nav-link" href="#vuelos">Vuelos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#bus">Bus</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#pack">Pack</a>
+            </li>
+            <li className="nav-item botonNav" onClick={cambiarEstado}>
+              Contactanos
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
